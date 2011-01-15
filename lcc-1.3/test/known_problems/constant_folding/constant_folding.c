3efe172140522ab111b03045638d8f18620db4e9
Edgar Lakis <edgar.lakis@gmail.com>
2011-01-15 22:23:05 +0000

    bug case added

--- lcc-1.3/test/known_problems/constant_folding/constant_folding.c
+++ lcc-1.3/test/known_problems/constant_folding/constant_folding.c
@@ -1,34 +1,72 @@
 /*
  * Status:
- * 	Workaround known
+ *	Workaround known
  * Description:
- * 	Unsigned constant folding
- * 	Fails to calculate correct folded value. Unsigned constants become
- * 	truncated at 8'th bit.
+ *	Constant folding. Fails to calculate correct folded value.
+ *	  1. Unsigned constants become truncated at 8'th bit. This happens
+ *	because lc3 has sizeof(int)==1, and lcc truncates unsigned constants
+ *	at 8*sizeof, to preserve semantics.
+ *	  2. Bitwise complement (BCOM) also fails the same way with signed constants.
+ *	From the source it seams that bitwise and (BAND) might also be affected.
+ * Source:
+ *	simplify() in src/simp.c:
+		  see:  l->u.v.i & ones(8*ty->size)
+		  in:
+		case CVI+U:
+		case CVU+U:
+		case BAND+U:
+		case BAND+I:
+		case BCOM+U:
+		case BCOM+I:
  * Workaround:
- *
+ *	1. Don't use constant expressions. Assign constant and compute
+ *	   expression in separate statements.
+ *	2. Don't use unsigned constants. Express the same value as signed
+ *	   (possibly negative) number. (Won't help with BCOM, BAND)
  */
 
 #include <stdio.h>
 
-#define BIG_CONST 0x00ffu
+#define UNSIGNED_CONST 0x00ffu
 //
 // Also doesn't work with any other constant bigger than 255
 //
-// #define BIG_CONST 0x0100u
-// #define BIG_CONST 0x1000u
-// #define BIG_CONST 0x8000u
+// #define UNSIGNED_CONST 0x0100u
+// #define UNSIGNED_CONST 0x1000u
+// #define UNSIGNED_CONST 0x8000u
 //
+//
+// But works fine if signed constants are used
+//
+// #define UNSIGNED_CONST 0x0100u
+// #define UNSIGNED_CONST 0x1000u
+// #define UNSIGNED_CONST 0x8000u
+//
+#define SIGNED_CONST 0x02ff
 
 int main() {
+	/*
+	 * Unsigned truncation problem
+	 */
+	int i = UNSIGNED_CONST+1;
 
-	int addr = BIG_CONST+1;
+	printf("1. Badd: %d %x \n", i, i);
 
-	printf("Badd: %d %x \n", addr, addr);
+	i = UNSIGNED_CONST;
+	i += 1;
+	printf("1. Good: %d %x \n", i, i);
 
-	addr = BIG_CONST;
-	addr += 1;
-	printf("Good: %d %x \n", addr, addr);
+	/*
+	 * Unsigned truncation problem
+	 */
+	i = ~SIGNED_CONST;
+
+	printf("2. Badd: %d %x \n", i, i);
+
+	i = SIGNED_CONST;
+	i = ~i;
+	printf("2. Good: %d %x \n", i, i);
+
 
 	return 0;
 }
