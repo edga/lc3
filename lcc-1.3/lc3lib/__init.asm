9614563d9d40aed43c3c916f07a5dbd8550239e6
Edgar Lakis <edgar.lakis@gmail.com>
2012-01-03 15:24:35 +0000

    HALT replaced with BUSY_LOOP in the the C program init

--- lcc-1.3/lc3lib/__init.asm
+++ lcc-1.3/lc3lib/__init.asm
@@ -8,14 +8,14 @@ ADD R6, R5, #0
 LD R4, GLOBAL_DATA_POINTER
 LD R7, GLOBAL_MAIN_POINTER
 jsrr R7
-HALT
 
-LD R7, SERIAL_MONITOR_ADDR
-jmp R7
+; The main function has finished, so we do nothing
+BUSY_LOOP: BR BUSY_LOOP
+
+
 GLOBAL_DATA_POINTER .FILL GLOBAL_DATA_START
 GLOBAL_MAIN_POINTER .FILL main
 STACK_ADDR .FILL xdfff
-SERIAL_MONITOR_ADDR .FILL x0200
 
 ;;;; END of the header ;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
